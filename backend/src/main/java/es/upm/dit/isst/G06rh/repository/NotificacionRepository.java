f2ae3d18f98db0638fa2c9f8267c8e211a0ed242
MiguiGH01 <105802045+MiguiGH01@users.noreply.github.com>
2023-04-17 09:09:05 +0200

    Delete backend directory

--- backend/src/main/java/es/upm/dit/isst/G06rh/repository/NotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.isst.G06rh.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import es.upm.dit.isst.G06rh.model.*;
-
-public interface NotificacionRepository extends CrudRepository<NOTIFICACION, Long> {
-     
-}
